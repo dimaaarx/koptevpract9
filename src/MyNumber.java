740bec04119a3a435863dc58bd6dcf0dfb1f8ac9
dimaaarx <koptevdima1107@gmail.com>
2026-06-09 23:14:22 +0300

    Update MyNumber.java

--- src/MyNumber.java
+++ src/MyNumber.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+public class MyNumber implements Comparable<MyNumber> {
+    private int value;
 
-public class MyNumber {
+    public MyNumber(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public int compareTo(MyNumber other) {
+        return this.value - other.value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
 }
