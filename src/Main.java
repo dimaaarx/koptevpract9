dd05ede56db98c3a724b08a8e1d2b73197b89aaa
dimaaarx <koptevdima1107@gmail.com>
2026-06-09 23:14:06 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            MyVector vector = new MyVector(5);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            vector.add(new MyNumber(30));
+            vector.add(new MyNumber(10));
+            vector.add(new MyNumber(50));
+            vector.add(new MyNumber(20));
+            vector.add(new MyNumber(40));
+
+            System.out.println("Original vector:");
+            vector.print();
+
+            vector.sort();
+            System.out.println("Sorted with Comparable:");
+            vector.print();
+
+            vector.sort(new DescendingComparator());
+            System.out.println("Sorted with Comparator:");
+            vector.print();
+
+        } catch (InvalidCapacityException e) {
+            System.out.println("Capacity error: " + e.getMessage());
+
+        } catch (InvalidIndexException e) {
+            System.out.println("Index error: " + e.getMessage());
+
+        } catch (NullValueException e) {
+            System.out.println("Null value error: " + e.getMessage());
+
+        } catch (VectorOverflowException e) {
+            System.out.println("Overflow error: " + e.getMessage());
         }
     }
 }
